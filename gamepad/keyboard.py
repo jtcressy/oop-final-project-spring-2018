417b193f24fd39dc5705590c79966d2f71b6974d
jtcressy <joel@jtcressy.net>
2018-04-10 20:04:14 -0600

    Defined keyboard methods - also implemented send_keypress

--- gamepad/keyboard.py
+++ gamepad/keyboard.py
@@ -1,4 +1,12 @@
 
 class Keyboard:
-    def __init__(self):
-        ""
+
+    def send_keypress(self, keycode):
+        """Send up then down keystroke for given keycode"""
+        print(keycode, end='')
+
+    def send_keydown(self, keycode):
+        """Send down keystroke for given keycode"""
+
+    def send_keyup(self, keycode):
+        """Send up keystroke for given keycode"""
